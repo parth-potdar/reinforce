289bf5384ffbb62ad40c70693adb80eea6d24a98
parth <parthmpotdar02@gmail.com>
2026-05-18 12:41:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# REINFORCE
+Implementing REINFORCE algorithm.
---
+++ reinforce/agent.py
@@ -0,0 +1,13 @@
+from reinforce.policy_network import PolicyNetwork
+
+class REINFORCEAgent:
+    def __init__(self):
+        """Initialise REINFORCE Agent"""
+
+        self.policy_network = PolicyNetwork()
+    
+    def select_action(self):
+        """Select action from current policy based on current state"""
+
+    def update(self):
+        """Update policy parameters"""
